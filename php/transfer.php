55fd9d5cbc3f3160036d47d2b19c6115a035e68b
adek4397 <adrianszczepanik2004@gmail.com>
2022-07-08 20:10:53 +0200

    święte czoło

--- php/transfer.php
+++ php/transfer.php
@@ -13,21 +13,23 @@
             $transfer_account_number = $_POST['transfer_account_number'];
             $transfer_sum = $_POST['transfer_sum'];
 
-            $conection = mysqli_connect($db_address, $db_user, $db_password, $db_name);
-    
-            if($conection){
-                // echo "connect";
-    
-                $id = $_SESSION["id"];
-    
-                echo "transfer ok";
+            if($transfer_account_number > 0 && $transfer_sum > 0){
+
+                $conection = mysqli_connect($db_address, $db_user, $db_password, $db_name);
+
+                if($conection){
+                    echo "connect";
+        
+                    $id = $_SESSION["id"];
+        
+                }
+                else{
+                    echo "false_conect";
+                    // echo "No connection with data base";
+                }
+        
+                mysqli_close($conection);
             }
-            else{
-                echo "false_conect";
-                // echo "No connection with data base";
-            }
-    
-            mysqli_close($conection);
         }    
         else{
             // echo "no user";
